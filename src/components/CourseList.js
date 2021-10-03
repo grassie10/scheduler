218d7038cd66139449a0bb83f774da4c38a54689
Grace Liu <gxliu1@gmail.com>
2021-10-03 00:17:10 -0500

    Modularize code

---
+++ src/components/CourseList.js
@@ -0,0 +1,40 @@
+import Course from './Course.js';
+import { getCourseTerm, terms } from '../utilities/times.js';
+import React, { useState } from 'react';
+
+const TermSelector = ({term, setTerm}) => (
+  <div className="btn-group">
+  { 
+    Object.values(terms)
+      .map(value => <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />)
+  }
+  </div>
+);
+
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" autoComplete="off" 
+    checked={checked} setTerm={setTerm} onChange={() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
+);
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+      { termCourses.map(course => <Course key={course.id} course={ course } 
+        selected={selected} setSelected={setSelected} />) }
+      </div>
+    </>
+  );
+};
+
+export default CourseList;
